3d41a489d43ab3f403f4dc13f9ac9d27632f2958
jack-thant <thanthtoo5728@gmail.com>
2024-05-17 09:09:26 +0800

    Completed Dashboard page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,6 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    // Parallel Data Fetching using Promise.all
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
